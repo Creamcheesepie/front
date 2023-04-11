cff34ab6430adf65042a411384d53245be1d75c2
chipang <gnldbs1004@naver.com>
2023-04-11 16:03:07 +0900

    2023-04-11 16:03

---
+++ 0412_JS/test8.js
@@ -0,0 +1,15 @@
+function atom3(MBC){;
+  document.write("이곳은 atom3 함수 안이다." + (MBC+300) + "<br/>");
+
+  console.log("이곳은 atom3 함수 안이에용" + (100+200));
+
+  alert("이곳은 atom3 함수 안이에요~" + (100+150)); 
+}
+
+function atom4(MBC){;
+  document.write("이곳은 atom3 함수 안이다." + (MBC+300) + "<br/>");
+
+  console.log("이곳은 atom3 함수 안이에용" + (100+200));
+
+  alert("이곳은 atom3 함수 안이에요~" + (100+150)); 
+}
